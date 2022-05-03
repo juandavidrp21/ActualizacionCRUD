cd031de44c8e87112167efb31cf5a618528e5306
Juan David Romero <juandavidrp21@gmail.com>
2022-05-03 08:59:43 +0200

    Actualizacion campo APELLIDO

---
+++ sistema/src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.empleadosBL;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Daniel
+ */
+public class empleadosBL {
+    
+    int ID;
+    String nombre, correo, apellido;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
